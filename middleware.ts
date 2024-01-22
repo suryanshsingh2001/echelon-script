b242d7364914b9dc4751160cbcf0549df16f3033
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-01-23 03:11:01 +0530

    FInally Fix publicRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhooks/clerk',
-        '/api/webhooks/stripe',
-        '/api/uploadthing',
-
-    ],
-    ignoredRoutes: [
-        '/api/webhooks/clerk',
-        '/api/webhooks/stripe',
-        '/api/uploadthing',
-    ],
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
  
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
