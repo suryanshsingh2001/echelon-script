5dd645326294175b204cc8befc15e3662455d858
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-01-24 02:18:25 +0530

    try to fix uploadthing

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -14,7 +14,7 @@ type FileUploaderProps = {
 }
 
 export function FileUploader({ imageUrl, onFieldChange, setFiles }: FileUploaderProps) {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+  const onDrop = useCallback((acceptedFiles: any) => {
     setFiles(acceptedFiles)
     onFieldChange(convertFileToUrl(acceptedFiles[0]))
   }, [])
