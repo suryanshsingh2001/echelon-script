dbe076503e0255376696be687a289355507fd3f7
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-01-30 01:55:44 +0530

    Add Create Event and Events Details page 3:27:27

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,11 +1,12 @@
-import React, { startTransition, useState } from "react";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "@/components/ui/select"
+import { ICategory } from "@/lib/database/models/category.model"
+import { startTransition, useEffect, useState } from "react"
 import {
   AlertDialog,
   AlertDialogAction,
@@ -16,57 +17,68 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
   AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
-
-import { ICategory } from "@/lib/database/models/category.model";
-import { Input } from "../ui/input";
+} from "@/components/ui/alert-dialog"
+import { Input } from "../ui/input"
+import { createCategory, getAllCategories } from "@/lib/actions/category.actions"
 
 type DropdownProps = {
-  value: string;
-  onChangeHandler?: () => void;
-};
+  value?: string
+  onChangeHandler?: () => void
+}
 
 const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
-  const [categories, setCategories] = useState<ICategory[]>([]);
-  const [newCategory, setNewCategory] = useState('')
+  const [categories, setCategories] = useState<ICategory[]>([])
+  const [newCategory, setNewCategory] = useState('');
 
   const handleAddCategory = () => {
-    
+    createCategory({
+      categoryName: newCategory.trim()
+    })
+      .then((category) => {
+        setCategories((prevState) => [...prevState, category])
+      })
   }
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const categoryList = await getAllCategories();
+
+      categoryList && setCategories(categoryList as ICategory[])
+    }
+
+    getCategories();
+  }, [])
+
   return (
     <Select onValueChange={onChangeHandler} defaultValue={value}>
       <SelectTrigger className="select-field">
         <SelectValue placeholder="Category" />
       </SelectTrigger>
       <SelectContent>
-        {categories.length > 0 &&
-          categories.map((category) => (
-            <SelectItem
-              key={category._id}
-              value={category._id}
-              className="select-item p-regular-14"
-            >
-              {category.name}
-            </SelectItem>
-          ))}
+        {categories.length > 0 && categories.map((category) => (
+          <SelectItem key={category._id} value={category._id} className="select-item p-regular-14">
+            {category.name}
+          </SelectItem>
+        ))}
+
         <AlertDialog>
-          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Open</AlertDialogTrigger>
+          <AlertDialogTrigger className="p-medium-14 flex w-full rounded-sm py-3 pl-8 text-primary-500 hover:bg-primary-50 focus:text-primary-500">Add new category</AlertDialogTrigger>
           <AlertDialogContent className="bg-white">
-            <AlertDialogHeader className="">
+            <AlertDialogHeader>
               <AlertDialogTitle>New Category</AlertDialogTitle>
               <AlertDialogDescription>
-               <Input type='text' placeholder="Category Name" className="input-field mt-3" onChange={(e) => setNewCategory(e.target.value)} />
+                <Input type="text" placeholder="Category name" className="input-field mt-3" onChange={(e) => setNewCategory(e.target.value)} />
               </AlertDialogDescription>
             </AlertDialogHeader>
             <AlertDialogFooter>
               <AlertDialogCancel>Cancel</AlertDialogCancel>
-              <AlertDialogAction onChangeCapture={()=>{startTransition(handleAddCategory)}}></AlertDialogAction>
+              <AlertDialogAction onClick={() => startTransition(handleAddCategory)}>Add</AlertDialogAction>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
       </SelectContent>
     </Select>
-  );
-};
+  )
+}
 
-export default Dropdown;
+export default Dropdown
--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -28,6 +28,11 @@ import { Checkbox } from "@/components/ui/checkbox";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
+import { useUploadThing } from "@/lib/uploadthing";
+import { handleError } from "@/lib/utils";
+import {useRouter} from "next/navigation";
+import { createEvent } from "@/lib/actions/event.actions";
+
 type EventFormProps = {
   userId: string;
   type: "Create" | "Update";
@@ -37,13 +42,41 @@ const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
   const initialValues = eventDefaultValues;
 
+  const router = useRouter();
+  const { startUpload } = useUploadThing("imageUploader");
+
   const form = useForm<z.infer<typeof eventFormSchema>>({
     resolver: zodResolver(eventFormSchema),
     defaultValues: initialValues,
   });
 
-  function onSubmit(values: z.infer<typeof eventFormSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    let uploadedImageUrl = values.imageUrl;
+
+    if (files.length > 0) {
+      const uploadedImages = await startUpload(files);
+
+      if (!uploadedImages) return;
+
+      uploadedImageUrl = uploadedImages[0].url;
+    }
+
+    if (type === "Create") {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl: uploadedImageUrl },
+          userId,
+          path: "/profile",
+        });
+
+        if (newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        handleError(error);
+      }
+    }
   }
   return (
     <Form {...form}>
@@ -283,7 +316,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               </FormItem>
             )}
           />
-           <FormField
+          <FormField
             control={form.control}
             name="url"
             render={({ field }) => (
@@ -309,15 +342,14 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             )}
           />
         </div>
-        <Button 
+        <Button
           type="submit"
           size="lg"
           disabled={form.formState.isSubmitting}
           className="button col-span-2 w-full"
         >
-          {form.formState.isSubmitting ? (
-            'Submitting...'
-          ): `${type} Event `}</Button>
+          {form.formState.isSubmitting ? "Submitting..." : `${type} Event `}
+        </Button>
       </form>
     </Form>
   );
