8b9c2aa0cf16531cf9d0ca18d4d9761300deaafe
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2024-02-03 21:20:16 +0530

    Fix MetaData

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,22 +16,6 @@ export const metadata: Metadata = {
   icons: {
     icon: "/assets/images/echelonscript-high-resolution-logo.png",
   },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://echelonscript.com",
-    siteName: "EchelonScript",
-    title: "EchelonScript",
-    description: "Empowering Devs, Hosting Dreams",
-    images: [
-      {
-        url: "/assets/images/echelonscript-high-resolution-logo.png",
-        width: 1200,
-        height: 630,
-        alt: "EchelonScript",
-      },
-    ],
-  }
 };
 
 export default function RootLayout({
